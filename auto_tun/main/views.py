3bd209fc6c4d9c362f6a5341957d7d6bc6c86d80
Michael Florya <mih2303@mail.ru>
2024-05-14 01:08:48 +0300

    create & config controller, url & template of index.html

--- auto_tun/main/views.py
+++ auto_tun/main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context = {
+        'title': 'Главная | Auto-Tun',
+    }
+    return render(request, 'main/index.html', context)
